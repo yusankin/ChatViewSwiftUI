e6f591db2ba42f007072765a68e63fde2a263d88
yusankin <masuda.yu.business@gmail.com>
2024-12-05 19:53:41 +0900

    first commit:ChatView アプリの初期レイアウト

--- ChatViewSwiftUI/ContentView.swift
+++ ChatViewSwiftUI/ContentView.swift
@@ -9,16 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0){
+            // Message Area
+            ScrollView{
+                VStack{
+                    ForEach(0..<15){ _ in
+                        HStack{
+                            Circle()
+                                .frame(width: 60, height: 60)
+                            Capsule()
+                                .frame(height: 60)
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 72)
+            }
+            .background(Color.cyan)
+            .overlay(
+                // Navigation Area
+                HStack{
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Text("Title")
+                    Spacer()
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Circle()
+                        .frame(width: 40, height: 40)
+                }
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.black.opacity(0.5))
+                , alignment: .top
+            )
+            
+            // Input Area
+            HStack{
+                Circle()
+                    .frame(width: 40, height: 40)
+                Circle()
+                    .frame(width: 40, height: 40)
+                Circle()
+                    .frame(width: 40, height: 40)
+                Capsule()
+                    .frame(height: 40)
+                Circle()
+                    .frame(width: 40, height: 40)
+                
+            }
+            .padding()
+            .background(Color.white)
         }
-        .padding()
     }
 }
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
