e3ac7cab65c117d14fdce8c19f88e51b7692ae10
yusankin <masuda.yu.business@gmail.com>
2024-12-06 13:18:40 +0900

    UI設計講座受講完了

--- ChatViewSwiftUI/Views/ContentView.swift
+++ ChatViewSwiftUI/Views/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var textFieldText: String = ""
+    
     var body: some View {
         VStack(spacing: 0){
             // Message Area
@@ -41,16 +44,25 @@ extension ContentView {
     
     private var inputArea: some View {
         HStack{
-            Circle()
-                .frame(width: 40, height: 40)
-            Circle()
-                .frame(width: 40, height: 40)
-            Circle()
-                .frame(width: 40, height: 40)
-            Capsule()
-                .frame(height: 40)
-            Circle()
-                .frame(width: 40, height: 40)
+            HStack{
+                Image(systemName: "plus")
+                Image(systemName: "camera")
+                Image(systemName: "photo")
+            }
+            .font(.title2)
+            TextField("Aa", text: $textFieldText)
+                .padding()
+                .background(Color(uiColor: .secondarySystemBackground))
+                .clipShape(Capsule())
+                .overlay(
+                    Image(systemName: "face.smiling")
+                        .font(.title2)
+                        .padding(.trailing)
+                        .foregroundColor(.gray)
+                    , alignment: .trailing
+                )
+            Image(systemName: "mic")
+                .font(.title2)
             
         }
         .padding()
@@ -59,19 +71,20 @@ extension ContentView {
     
     private var navigationArea: some View {
         HStack{
-            Circle()
-                .frame(width: 40, height: 40)
+            Image(systemName: "chevron.backward")
+                .font(.title2)
             Text("Title")
+                .font(.title2.bold())
             Spacer()
-            Circle()
-                .frame(width: 40, height: 40)
-            Circle()
-                .frame(width: 40, height: 40)
-            Circle()
-                .frame(width: 40, height: 40)
+            HStack(spacing: 16) {
+                Image(systemName: "text.magnifyingglass")
+                Image(systemName: "phone")
+                Image(systemName: "line.3.horizontal")
+            }
+            .font(.title2)
+            
         }
-        .foregroundColor(Color.white)
         .padding()
-        .background(Color.black.opacity(0.5))
+        .background(Color.cyan.opacity(0.9))
     }
 }
--- ChatViewSwiftUI/Views/MessageRow.swift
+++ ChatViewSwiftUI/Views/MessageRow.swift
@@ -9,16 +9,50 @@ import SwiftUI
 
 struct MessageRow: View {
     var body: some View {
-        HStack{
-            Circle()
-                .frame(width: 60, height: 60)
-            Capsule()
-                .frame(height: 60)
+        HStack(alignment: .top){
+            userThum
+            messageText
+            messageState
+            Spacer()
         }
         .padding(.bottom)
     }
 }
 
+extension MessageRow{
+    
+    private var userThum: some View {
+        Image("user01")
+            .resizable()
+            .frame(width: 48, height: 48)
+            .clipShape(Circle())
+    }
+    
+    private var messageText: some View {
+        Text("Hello World")
+            .padding()
+            .background(Color.white)
+            .cornerRadius(30)
+    }
+    
+    private var messageState: some View {
+        VStack(alignment: .trailing){
+            Spacer()
+            Text("既読")
+            Text(formattedDateString)
+        }
+        .foregroundColor(.secondary)
+        .font(.footnote)
+    }
+    
+    private var formattedDateString: String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: Date())
+    }
+}
+
 #Preview {
     MessageRow()
+        .background(Color.cyan)
 }
